698d2208c032598b078d45716a2f65b57a09c0e0
KamoLee <xu3u835k6@gmail.com>
2024-01-29 10:14:21 +0800

    feat: modify PrivateRoute. check is login by useUserStore

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,41 +1,43 @@
 import { useNavigate, Outlet } from "react-router-dom"
 import { useEffect, useState } from "react";
 
-const loginStatus = false
+import useUserStore from "@/store/useUserStore";
 
 const AuthRoute = () => {
-    const [isLogin] = useState<boolean>(loginStatus)
+    //const [isLogin] = useState<boolean>(loginStatus)
     const navigate = useNavigate()
+    const token = useUserStore(s => s.token)
+
+    console.log(token)
 
     useEffect(() => {
-        console.log('AuthRoute', isLogin)
-        if (!isLogin) {
+        if (!token) {
             navigate('/login')
         }
     }, [])
 
     return (
         <>
-            Auth Route
             <Outlet />        
         </>
     )
 }
 
 const NonAuthRoute = () => {
-    const [isLogin] = useState<boolean>(loginStatus)
-
+    //const [isLogin] = useState<boolean>(loginStatus)
+    const token = useUserStore(s => s.token)
     const navigate = useNavigate()
 
+    console.log(token)
+
     useEffect(() => {
-        if (isLogin) {
+        if (token) {
             navigate('/index')
         }
     }, [])
 
     return (
         <>
-            Non Auth Route
             <Outlet />        
         </>
     )
