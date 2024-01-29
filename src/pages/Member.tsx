5fe435852691628a0f15f1ac38577425be1db7ee
leo.zheng <leozheng@ad2iction.com>
2024-01-29 16:29:08 +0800

    feature: add member basic setting

--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -1,11 +1,45 @@
-import Layout from "@/components/Layout"
-
-const Member = () => {
-    return (
-        <Layout>
-            <h2>Member</h2>
-        </Layout>
-    )
+interface SectionItem {
+  id: string;
+  label: string;
+  value: string;
 }
+import { useState } from 'react';
+import Layout from '@/components/Layout';
+import { Outlet } from 'react-router-dom';
+import SvgIcon from '@/components/SvgIcon';
+import SectionTab from '@/components/SectionTab';
+const Member = () => {
+  const sectionList: SectionItem[] = [
+    {
+      id: '1',
+      label: '個人資料',
+      value: '0'
+    },
+    {
+      id: '2',
+      label: '我的訂單',
+      value: '1'
+    }
+  ];
+  const [activeItem, setActiveItem] = useState('0');
+  return (
+    <Layout>
+      <div className="member">
+        <div className="hero-area">
+          <div className="hero-area-wrapper">
+            <SvgIcon name="svg/ic_user" className="hero-area-icon"></SvgIcon>
+            <p className="hero-area-text tw-text-white tw-font-[700] tw-text-[32px] md:tw-text-[48px] tw-tracking-[1.6px] md:tw-tracking-[2.4px]">
+              Hello，Leo
+            </p>
+          </div>
+        </div>
+        <div className="tab-area">
+          <SectionTab sectionList={sectionList} activeItemValue={activeItem} onChange={setActiveItem} />
+        </div>
+        <Outlet />
+      </div>
+    </Layout>
+  );
+};
 
-export default Member
+export default Member;
---
+++ src/pages/Member/Info.tsx
@@ -0,0 +1,9 @@
+const MemberInfo = () => {
+  return (
+    <div className="member-info">
+      <div className="container">Member Info</div>
+    </div>
+  );
+};
+
+export default MemberInfo;
