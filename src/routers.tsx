60c84d1a9585516c6bc564132da304973ac74117
leo.zheng <leozheng@ad2iction.com>
2024-01-30 22:03:22 +0800

    style: prettier change

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,14 +1,14 @@
-import { createHashRouter } from "react-router-dom"
-import Index from '@/pages/Index.tsx'
-import Login from '@/pages/Login.tsx'
-import Register from "@/pages/Register.tsx"
-import Member from "@/pages/Member.tsx"
+import { createHashRouter } from 'react-router-dom';
+import Index from '@/pages/Index.tsx';
+import Login from '@/pages/Login.tsx';
+import Register from '@/pages/Register.tsx';
+import Member from '@/pages/Member.tsx';
 import MemberInfo from '@/pages/Member/Info';
-import Rooms from '@/pages/Rooms.tsx'
-import NotFound from "@/pages/NotFound.tsx"
-import BookRoom from "@/pages/BookRoom.tsx"
-import BookingSuccess from "@/pages/BookingSuccess.tsx"
-import { AuthRoute, NonAuthRoute } from '@/components/PrivateRoute.tsx'
+import Rooms from '@/pages/Rooms.tsx';
+import NotFound from '@/pages/NotFound.tsx';
+import BookRoom from '@/pages/BookRoom.tsx';
+import BookingSuccess from '@/pages/BookingSuccess.tsx';
+import { AuthRoute, NonAuthRoute } from '@/components/PrivateRoute.tsx';
 
 const routers = createHashRouter([
   {
@@ -36,35 +36,37 @@ const routers = createHashRouter([
             element: <MemberInfo />
           }
         ]
-    },
-    {
-        path: "/",
-        // element: <AuthRoute />,
-        children: [
-            {
-                path: "/BookingSuccess",
-                element: <BookingSuccess />,
-            },
-        ]
-    },
-    {
-        path: "/",
-        element: <NonAuthRoute />,
-        children: [
-            {
-                path: "/Login",
-                element: <Login />,
-            },
-            {
-                path: "/Register",
-                element: <Register />,
-            },
-        ]
-    },
-    {
-        path: "*",
-        element: <NotFound />,
-    },
+      }
+    ]
+  },
+  {
+    path: '/',
+    // element: <AuthRoute />,
+    children: [
+      {
+        path: '/BookingSuccess',
+        element: <BookingSuccess />
+      }
+    ]
+  },
+  {
+    path: '/',
+    element: <NonAuthRoute />,
+    children: [
+      {
+        path: '/Login',
+        element: <Login />
+      },
+      {
+        path: '/Register',
+        element: <Register />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
 ]);
 
 // Page     NonLogin    Login
