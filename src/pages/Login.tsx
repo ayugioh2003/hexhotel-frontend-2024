d0a2d85780f11fdc8932f5b922a9b71a28df38fd
KamoLee <xu3u835k6@gmail.com>
2024-02-04 23:15:50 +0800

    feat: modify Login, Register Layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,20 +4,23 @@ import { useNavigate, Link } from 'react-router-dom'
 import { login } from '@/services/UserService'
 import useUserStore from "@/store/useUserStore"
 import bgLogin from '@/assets/png/bg_login.png'
-
 const Login = () => {
     const [email, setEmail] = useState('')
+    const [emailValidator, setEmailValidator] = useState('')
     const [password, setPassword] = useState('')
+    const [passwordValidator, setPasswordValidator] = useState('')
     const [isRemember, setIsRemember] = useState(false)
     const setUser = useUserStore(s => s.setUser)
     const navigate = useNavigate()
 
     const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
+        validateEmail(e.target.value)
     }
 
     const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
         setPassword(e.target.value)
+        validatePassword(e.target.value)
     }
 
     const handleChangeIsRemember = () => {
@@ -25,62 +28,101 @@ const Login = () => {
     }
 
     const handleLogin = async () => {
+        if(!validInput()) {
+            return
+        }
         try {
             const response = await login({
                 email, password
             })
             setUser(response)
             navigate('/index')
-        } catch (ex) {
+        } catch (ex: any) {
+            setEmailValidator(ex?.message)
             console.error(ex)
         }
     }
 
-    const style = {
-        paddingTop: "120px",
-        background: "#140F0A",
-        minHeight: "100vh"        
+    const validInput = () => {
+        let isValid = true
+        isValid = validateEmail(email) && isValid
+        isValid = validatePassword(password) && isValid
+        return isValid
     }
 
-
-    const basis0 = {
-        "flexBasis": "0",
+    const validateEmail = (email: string) => {
+        let isValid = true
+        if(email) {
+            setEmailValidator('')
+        } else {
+            isValid = false
+            setEmailValidator('請輸入電子信箱')
+        }
+        return isValid
     }
 
+    const validatePassword = (password: string) => {
+        let isValid = true
+        if(password) {
+            setPasswordValidator('')
+        } else {
+            isValid = false
+            setPasswordValidator('請輸入密碼')
+        }
+        return isValid
+    }
 
     return (
         <Layout showFooter={false}>
-            <div style={style} className="test d-flex">
-                <div style={basis0} className="flex-grow-1 flex-bais-0 d-none d-md-block">
-                    <img src={bgLogin} alt="login backgound" style={{width: "100%", display: 'block'}}/>
+            <div className="min-vh-100 row g-0 dark-background">
+                <div className="col-md-6 d-none d-md-block">
+                    <img 
+                        src={bgLogin} 
+                        alt="login backgound"
+                        className="w-100 vh-100 object-fit-cover"
+                    />
                 </div>
-                <div  style={basis0} className="flex-grow-1 flex-bais-0 d-flex justify-content-center align-items-center">
-                    <div className="d-flex flex-column justify-content-center align-items-stretch">
+                <div className="col-md-6 vh-100 p-3 d-flex justify-content-center align-items-center">
+                    <form className="d-flex flex-column justify-content-center align-items-stretch">
                         <p className="text-primary mb-2">享樂酒店，誠摯歡迎</p>
                         <h2 className="h1 text-white mb-4">立即開始旅程</h2>
                         <label htmlFor="email" className="text-white mb-1">電子信箱</label>
                         <input 
                             type="text" id="email" 
-                            className="form-control mb-2" 
+                            className={`form-control ${emailValidator ? 'is-invalid' : ''}`} 
                             value={email} 
+                            autoComplete="username"
                             onChange={handleChangeEmail}
                             />
-                        <label htmlFor="password" className="text-white mb-1">密碼</label>
+                        {
+                            emailValidator &&
+                            <div className="invalid-feedback mt-1">
+                                {emailValidator}
+                            </div>
+                        }
+                        <label htmlFor="password" className="text-white mb-1 mt-3">密碼</label>
                         <input 
                             type="password" id="password" 
-                            className="form-control mb-2" 
+                            className={`form-control ${passwordValidator ? 'is-invalid' : ''}`} 
                             value={password} 
+                            autoComplete="current-password"
                             onChange={handleChangePassword} 
                             />
-                        <div className="w-100 d-flex justify-content-between mb-3">
+                        {
+                            passwordValidator &&
+                            <div className="invalid-feedback mt-1">
+                                {passwordValidator}
+                            </div>
+                        }
+                        <div className="w-100 d-flex justify-content-between mt-3">
                             <label htmlFor="remember" className="text-white">
                                 <input type="checkbox" name="remember" id="remember" checked={isRemember} onChange={handleChangeIsRemember}/> 記住帳號
                             </label>
                             <a href="#">忘記密碼</a>
                         </div>
-                        <button type="button" className="btn btn-primary w-100 mb-3" onClick={handleLogin}>會員登入</button>
-                        <p className="mb-0 text-white">沒有會員嗎？ <Link to="/register">前往註冊</Link></p>
-                    </div>
+                        <button type="button" className="btn btn-primary w-100 mt-4" onClick={handleLogin}>會員登入</button>
+                        <p className="mb-0 mt-4 text-white">沒有會員嗎？ <Link to="/register">前往註冊</Link></p>
+                    </form>
                 </div>
             </div>
         </Layout>        
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/Layout"
-import { useState, ChangeEvent, useEffect } from "react"
+import { useState, ChangeEvent } from "react"
 import { signup } from '@/services/UserService'
 import useUserStore from "@/store/useUserStore"
 import { useNavigate } from 'react-router-dom'
@@ -21,17 +21,17 @@ interface RegisterData {
     };
 }
 
-// interface RegisterValidateData {
-//     // name: string;
-//     email: string;
-//     // password: string;
-//     // confirmPassword: string;
-//     // phone: string;
-//     // birthday: string;
-//     // address: {
-//     //     zipcode: number;
-//     //     detail: string;
-//     // };
+interface Step1Validate {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+// interface Step2Validate {
+//     name: string;
+//     phone: string;
+//     birthday: string;
+//     zipcode: string;
+//     detail: string;
 // }
 
 enum RegisterStepEnum {
@@ -53,18 +53,18 @@ const Register = () => {
             "detail": ""
         }
     })
-    const [validate, setValidate] = useState<RegisterData>({
-        "name": "",
+    const [step1Validate, setStep1Validate] = useState<Step1Validate>({
         "email": "",
         "password": "",
         "confirmPassword": "",
-        "phone": "",
-        "birthday": "",
-        "address": {
-            "zipcode": 0,
-            "detail": ""
-        }
     })
+    // const [step2Validate, setStep2Validate] = useState<Step2Validate>({
+    //     "name": "",
+    //     "phone": "",
+    //     "birthday": "",
+    //     "zipcode": "",
+    //     "detail": ""
+    // })
 
     const setUser = useUserStore(s => s.setUser)
     const navigate = useNavigate()
@@ -75,42 +75,8 @@ const Register = () => {
             ...prev,
             [name]: value,
         }))
-        checkInput(name, value)
     }
 
-    useEffect(() => {
-
-    }, [data])
-
-    const checkInput = (key: string, value: string) => {
-        let error = ""
-        if(key === "name") {
-            error = value ? "": "請輸入姓名"
-        } else if(key === "email") {
-            error = value ? "": "請輸入電子信箱"
-        } else if(key === "password") {
-            if(!value) {
-                error = "請輸入密碼"
-            } else if (data.confirmPassword && data.confirmPassword != value) {
-                error = "密碼與確認密碼不同"
-            } else {
-                error = ""
-            }
-        } else if(key === "confirmPassword") {
-            if(!value) {
-                error = "請輸入確認密碼"
-            } else if(data.password !== value) {
-                error = "密碼與確認密碼不同"
-            } else {
-                error = ""
-            }
-        }
-        setValidate(prev => ({
-            ...prev,
-            [key]: error,
-        }))
-    }
-    
     const handleRegister = async () => {
         try {
             const response = await signup({
@@ -158,17 +124,19 @@ const Register = () => {
                 detail: detail,
             },
         }))
-    } 
+    }
 
     const handleClickStep1 = async () => {
-        // 驗證 email
+        // if(!validateStep1()) {
+        //     return
+        // }
         try {
             const requestData = {
                 email: data.email
             }
             const res = await verifyEmailExist(requestData)
             if(res.result.isEmailExists) {
-                setValidate(prev => ({
+                setStep1Validate(prev => ({
                     ...prev,
                     email: '電子信箱已存在'
                 }))
@@ -176,111 +144,105 @@ const Register = () => {
                 setStep(RegisterStepEnum.second)
             }
         } catch (ex: any) {
-            setValidate(prev => ({
+            setStep1Validate(prev => ({
                 ...prev,
                 email: ex?.message
             }))
         }
     }
 
-    const style = {
-        paddingTop: "120px",
-        background: "#140F0A",
-        minHeight: "100vh"        
-    }
-
-    const basis0 = {
-        "flexBasis": "0",
-    }
-
     return (
         <Layout showFooter={false}>
-            <div style={style} className="test d-flex">
-                <div style={basis0} className="flex-grow-1 flex-bais-0 d-none d-md-block">
-                    <img src={bgLogin} alt="login backgound" style={{width: "100%", display: 'block'}}/>
+            <div className="min-vh-100 row g-0 dark-background">
+                <div className="col-md-6 d-none d-md-block">
+                <img
+                        src={bgLogin}
+                        alt="login backgound"
+                        className="w-100 vh-100 object-fit-cover"
+                    />
                 </div>
-                <div style={basis0}  className="flex-grow-1 flex-bais-0 d-flex justify-content-center align-items-center">
-                    <div className="d-flex flex-column justify-content-center align-items-stretch">
+                <div className="col-md-6 vh-100 p-3 d-flex justify-content-center align-items-center">
+                    <form className="d-flex flex-column justify-content-center align-items-stretch">
                         <p className="text-primary mb-2">享樂酒店，誠摯歡迎</p>
-                        <h2 className="h1 text-white mb-4">立即註冊</h2>
+                        <h2 className="h1 text-white mb-4">立即開始旅程</h2>
                     {
-                        step === RegisterStepEnum.first ? 
+                        step === RegisterStepEnum.first ?
                         <>
                             <label htmlFor="email" className="text-white mb-1">電子信箱</label>
-                            <input 
-                                type="text" name="email" id="email"
-                                className={`form-control ${validate.email ? 'is-invalid' : ''}`} 
-                                value={data.email} 
+                            <input
+                                type="text" name="email" id="email"  autoComplete="username"
+                                className={`form-control ${step1Validate.email ? 'is-invalid' : ''}`}
+                                value={data.email}
                                 onChange={handleChangeData}
                             />
                             {
-                                validate.email &&
-                                <div className="invalid-feedback">
-                                    {validate.email}
+                                step1Validate.email &&
+                                <div className="mt-1 invalid-feedback">
+                                    {step1Validate.email}
                                 </div>
                             }
                             <label htmlFor="password" className="text-white mt-2 mb-1">密碼</label>
-                            <input 
-                                type="password" name="password" id="password" 
-                                className={`form-control ${validate.password ? 'is-invalid' : ''}`} 
-                                value={data.password} 
-                                onChange={handleChangeData} 
+                            <input
+                                type="password" name="password" id="password" autoComplete="new-password"
+                                className={`form-control ${step1Validate.password ? 'is-invalid' : ''}`}
+                                value={data.password}
+                                onChange={handleChangeData}
                             />
                             {
-                                validate.password &&
-                                <div className="invalid-feedback">
-                                    {validate.password}
+                                step1Validate.password &&
+                                <div className="mt-1 invalid-feedback">
+                                    {step1Validate.password}
                                 </div>
                             }
                             <label htmlFor="confirmPassword" className="text-white mt-2 mb-1">確認密碼</label>
-                            <input 
-                                type="password" name="confirmPassword" id="confirmPassword" 
-                                className={`form-control ${validate.confirmPassword ? 'is-invalid' : ''}`} 
-                                value={data.confirmPassword} 
-                                onChange={handleChangeData} 
+                            <input
+                                type="password" name="confirmPassword" id="confirmPassword" autoComplete="new-password"
+                                className={`form-control ${step1Validate.confirmPassword ? 'is-invalid' : ''}`}
+                                value={data.confirmPassword}
+                                onChange={handleChangeData}
                             />
                             {
-                                validate.confirmPassword &&
-                                <div className="invalid-feedback">
-                                    {validate.confirmPassword}
+                                step1Validate.confirmPassword &&
+                                <div className="mt-1 invalid-feedback">
+                                    {step1Validate.confirmPassword}
                                 </div>
                             }
-                            <button 
-                                type="button" 
-                                className="btn btn-primary mt-2 w-100"
+                            <button
+                                type="button"
+                                className="mt-4 btn btn-primary mt-2 w-100"
                                 onClick={handleClickStep1}
                             >下一步</button>
                         </>:
                         <>
                             <label htmlFor="name" className="text-white mb-1">姓名</label>
-                            <input 
+                            <input
                                 type="text" name="name" id="name"
-                                className="form-control mb-2" 
-                                value={data.name} 
+                                className="form-control mb-2"
+                                value={data.name}
                                 onChange={handleChangeData}
                             />
                             <label htmlFor="phone" className="text-white mb-1">手機號碼</label>
-                            <input 
+                            <input
                                 type="text" name="phone" id="phone"
-                                className="form-control mb-2" 
-                                value={data.phone} 
+                                className="form-control mb-2"
+                                value={data.phone}
                                 onChange={handleChangeData}
                             />
                             <label className="text-white mb-1">生日</label>
                             <BirthdaySelector onChange={handleChangeBirthday}/>
                             <label className="text-white mb-1">地址</label>
                             <CitySelector onChange={handleChangeCity}/>
-                            <input 
-                                type="text" name="detail" id="detail" 
-                                className="form-control mt-1 mb-3" 
-                                value={data.address.detail} 
-                                onChange={e => handleChangeDetail(e.target.value)} 
+                            <input
+                                type="text" name="detail" id="detail"
+                                className="form-control mt-1 mb-3"
+                                value={data.address.detail}
+                                onChange={e => handleChangeDetail(e.target.value)}
                             />
 
                             <button type="button" className="btn btn-primary" onClick={handleRegister}>完成註冊</button>
-                        </>                
+                        </>
                     }
-                    </div>
+                    </form>
                 </div>
             </div>
         </Layout>
