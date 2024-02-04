36c5cf371b0cefd4fe2c44cd8ef931a2a046d34d
KamoLee <xu3u835k6@gmail.com>
2024-02-05 00:41:19 +0800

    fix: uncomment svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // vite-plugin-svg-icons
-//import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 
 // npm i -D @types/node
@@ -11,11 +11,11 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [
     react(),
-    // createSvgIconsPlugin({
-    //   iconDirs: [path.resolve(process.cwd(), 'src/assets/')],
-    //   symbolId: '[dir]/[name]',
-    //   customDomId: '__svg__icons__dom__',
-    // }),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/')],
+      symbolId: '[dir]/[name]',
+      customDomId: '__svg__icons__dom__',
+    }),
   ],
   resolve: {
     alias: [{ 
