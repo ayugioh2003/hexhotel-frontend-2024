6fe9336ced587bb335deae56d84463350909dd4e
KamoLee <xu3u835k6@gmail.com>
2024-02-05 00:39:02 +0800

    fix: modify register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -26,13 +26,11 @@ interface Step1Validate {
     password: string;
     confirmPassword: string;
 }
-// interface Step2Validate {
-//     name: string;
-//     phone: string;
-//     birthday: string;
-//     zipcode: string;
-//     detail: string;
-// }
+interface Step2Validate {
+    name: string;
+    phone: string;
+    detail: string;
+}
 
 enum RegisterStepEnum {
     first,
@@ -58,13 +56,12 @@ const Register = () => {
         "password": "",
         "confirmPassword": "",
     })
-    // const [step2Validate, setStep2Validate] = useState<Step2Validate>({
-    //     "name": "",
-    //     "phone": "",
-    //     "birthday": "",
-    //     "zipcode": "",
-    //     "detail": ""
-    // })
+
+    const [step2Validate, setStep2Validate] = useState<Step2Validate>({
+        "name": "",
+        "phone": "",
+        "detail": ""
+    })
 
     const setUser = useUserStore(s => s.setUser)
     const navigate = useNavigate()
@@ -78,6 +75,11 @@ const Register = () => {
     }
 
     const handleRegister = async () => {
+        const res = validateStep2()
+        setStep2Validate(res)
+        if(Object.values(res).some(m => m)) {
+            return
+        }
         try {
             const response = await signup({
                 name: data.name,
@@ -92,59 +94,15 @@ const Register = () => {
             })
             setUser(response)
             navigate('/index')
-        } catch (ex) {
-            console.error(ex)
-        }
-      });
-      setUser(response);
-      navigate('/index');
-    } catch (ex) {
-      console.error(ex);
-    }
-  };
+        } catch (ex: any) {
+            setStep(RegisterStepEnum.first)
+            setStep1Validate(prev => ({
+                ...prev,
+                email: ex?.message
+            }))
+        }      
+    };
 
-  return (
-    <Layout>
-      <div style={{ paddingTop: '120px' }}>
-        <h2>Register</h2>
-        {step === RegisterStepEnum.first ? (
-          <>
-            <label>電子信箱</label>
-            <input type="text" value={data.email} name="email" onChange={handleChangeData} />
-            <br />
-            <label>密碼</label>
-            <input type="password" value={data.password} name="password" onChange={handleChangeData} />
-            <br />
-            <label>確認密碼</label>
-            <input type="password" value={data.confirmPassword} name="confirmPassword" onChange={handleChangeData} />
-            <br />
-            <button type="button" onClick={() => setStep(RegisterStepEnum.second)}>
-              下一步
-            </button>
-          </>
-        ) : (
-          <>
-            <label>姓名</label>
-            <input type="text" value={data.name} name="name" onChange={handleChangeData} />
-            <br />
-            <label>手機號碼</label>
-            <input type="text" value={data.phone} name="phone" onChange={handleChangeData} />
-            <br />
-            <label>生日</label>
-            <input type="text" value={data.birthday} name="birthday" onChange={handleChangeData} />
-            <br />
-            {/* <label>地址</label>
-                    <input type="text" value={data.address} name="name"  onChange={handleChangeData}/>
-                    <br /> */}
-            <button type="button" onClick={handleRegister}>
-              完成註冊
-            </button>
-          </>
-        )}
-      </div>
-    </Layout>
-  );
-};
     const handleChangeBirthday = (birthday: string) => {
         setData(prev => ({
             ...prev,
@@ -175,9 +133,11 @@ const Register = () => {
     }
 
     const handleClickStep1 = async () => {
-        // if(!validateStep1()) {
-        //     return
-        // }
+        const res = validateStep1()
+        setStep1Validate(res)
+        if(Object.values(res).some(m => m)) {
+            return
+        }
         try {
             const requestData = {
                 email: data.email
@@ -196,6 +156,57 @@ const Register = () => {
                 ...prev,
                 email: ex?.message
             }))
+        }
+    }
+
+    const validateStep1 = (): Step1Validate => {
+        let email = ''
+        let password = ''
+        let confirmPassword = ''
+        if(!data.email) {
+            email = "請輸入電子信箱"
+        }
+
+        if(!data.password) {
+            password = "請輸入密碼"
+        }
+
+        if(!data.confirmPassword) {
+            confirmPassword = "請輸入確認密碼"
+        }
+
+        if(data.password && data.confirmPassword && data.password !== data.confirmPassword) {
+            confirmPassword = "密碼與確認密碼不同"
+        }
+
+        return {
+            email,
+            password,
+            confirmPassword
+        }
+    }
+
+    const validateStep2 = (): Step2Validate => {
+        let name = ''
+        let phone = ''
+        let detail = ''
+        if(!data.name) {
+            name = "請輸入姓名"
+        }
+
+        if(!data.phone) {
+            phone = "請輸入手機號碼"
+        }
+
+        if(!data.address.detail) {
+            detail = "請輸入地址"
+        }
+
+
+        return {
+            name,
+            phone,
+            detail
         }
     }
 
@@ -265,29 +276,46 @@ const Register = () => {
                             <label htmlFor="name" className="text-white mb-1">姓名</label>
                             <input
                                 type="text" name="name" id="name"
-                                className="form-control mb-2"
+                                className={`form-control ${step2Validate.name ? 'is-invalid' : ''}`}
                                 value={data.name}
                                 onChange={handleChangeData}
                             />
-                            <label htmlFor="phone" className="text-white mb-1">手機號碼</label>
+                            {
+                                step2Validate.name &&
+                                <div className="mt-1 invalid-feedback">
+                                    {step2Validate.name}
+                                </div>
+                            }                            
+                            <label htmlFor="phone" className="text-white mt-2 mb-1">手機號碼</label>
                             <input
                                 type="text" name="phone" id="phone"
-                                className="form-control mb-2"
+                                className={`form-control ${step2Validate.phone ? 'is-invalid' : ''}`}
                                 value={data.phone}
                                 onChange={handleChangeData}
                             />
-                            <label className="text-white mb-1">生日</label>
+                            {
+                                step2Validate.phone &&
+                                <div className="mt-1 invalid-feedback">
+                                    {step2Validate.phone}
+                                </div>
+                            }                            
+                            <label className="text-white mt-2 mb-1">生日</label>
                             <BirthdaySelector onChange={handleChangeBirthday}/>
-                            <label className="text-white mb-1">地址</label>
+                            <label className="text-white mt-2 mb-1">地址</label>
                             <CitySelector onChange={handleChangeCity}/>
                             <input
                                 type="text" name="detail" id="detail"
-                                className="form-control mt-1 mb-3"
+                                className={`mt-1 form-control ${step2Validate.detail ? 'is-invalid' : ''}`}
                                 value={data.address.detail}
                                 onChange={e => handleChangeDetail(e.target.value)}
                             />
-
-                            <button type="button" className="btn btn-primary" onClick={handleRegister}>完成註冊</button>
+                            {
+                                step2Validate.detail &&
+                                <div className="mt-1 invalid-feedback">
+                                    {step2Validate.detail}
+                                </div>
+                            }
+                            <button type="button" className="btn btn-primary mt-4" onClick={handleRegister}>完成註冊</button>
                         </>
                     }
                     </form>
